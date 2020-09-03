63506b6ed146f1adc408b65a9b0ffcd7fcbb91ce
Gavin Mendel-Gleason <gavin@terminusdb.com>
2020-09-03 06:54:00 +0200

    Fix query speed

--- wordnet/wordnet_query.py
+++ wordnet/wordnet_query.py
@@ -13,9 +13,9 @@ client.connect(db=db, key=key, account=account, user=user , insecure=True)
 
 def lookup_definition(Word):
     result = WQ().woql_and(
-        WQ().triple("v:X", "ontolex:canonicalForm", "v:_Blank"),
         WQ().triple("v:_Blank", "ontolex:writtenRep", {'@value' : Word,
                                                        '@language' : 'en'}),
+        WQ().triple("v:X", "ontolex:canonicalForm", "v:_Blank"),
         WQ().triple("v:X", "ontolex:sense", "v:Lemma"),
         WQ().triple("v:Lemma", "ontolex:isLexicalizedSenseOf", "v:PWN"),
         WQ().triple("v:PWN", "wn:definition", "v:_Blank2"),
