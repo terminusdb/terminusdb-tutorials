4353fcc4d44828119e7d608855e030517207d813
Mario García <iscmariog@gmail.com>
2021-10-18 10:31:37 -0500

    Tutorial updated

--- netflix/netflix.py
+++ netflix/netflix.py
@@ -4,35 +4,36 @@ from terminusdb_client.woqlschema.woql_schema import (
     DocumentTemplate,
     EnumTemplate,
     WOQLSchema,
+    LexicalKey,
 )
 
 import pandas as pd
 from tqdm import tqdm
 import tempfile
 
-my_schema = WOQLSchema()
+schema = WOQLSchema()
 
 class Netflix(DocumentTemplate):
-    _schema = my_schema
+    _schema = schema
     title: str
     type_of: "Content_Type"
-    director: str
-    cast: str
-    country: "Country"
+    director: Optional[str]
+    cast: Optional[str]
+    country: Optional[str]
     release_year: int
     rating: "Rating"
     duration: str
     listed_in: str
     description: str
-    date_added: str
+    date_added: Optional[str]
 
 class Content_Type(EnumTemplate):
-    _schema = my_schema
+    _schema = schema
     TV_Show = "TV Show"
     Movie = "Movie"
 
 class Rating(EnumTemplate):
-    _schema = my_schema
+    _schema = schema
     TV_MA = "TV-MA"
     R = ()
     PG_13 = "PG-13"
@@ -50,69 +51,41 @@ class Rating(EnumTemplate):
     TV_Y7_FV = "TV-Y7-FV"
     UR = ()
 
-class Country(DocumentTemplate):
-    _subdocument = []
-    _schema = my_schema
-    name: str
-
 def insert_data(client, url):
     df = pd.read_csv(url, chunksize=1000)
     for chunk in tqdm(df, desc='Transfering data'):
         csv = tempfile.NamedTemporaryFile()
         chunk.to_csv(csv)
         netflix_content = read_data(csv.name)
-        client.insert_document(netflix_content,
-                               commit_msg="Adding all Netflix content")
-
+        client.insert_document(netflix_content, commit_msg="Adding all Netflix content")
+        
 def read_data(csv):
     records = []
     df = pd.read_csv(csv)
-    selection = df.fillna('')
-    for index, row in selection.iterrows():
+    for index, row in df.iterrows():
 
         type_of = row['type'].replace(" ", "_")
-        rating = "NR" if row['rating'] == "" else row['rating'].replace("-", "_")
+        rating = "NR" if pd.isna(row['rating']) else row['rating'].replace("-", "_")
 
-        #Country
-        country = Country()
-        country.name = row['country']
-        records.append(country)
-
-        # Netflix
-        netflix = Netflix()
-        netflix.title = row['title']
-        netflix.type_of = Content_Type[type_of]
-        netflix.director = row['director']
-        netflix.cast = row['cast']
-        netflix.country = country
-        netflix.release_year = row['release_year']
-        netflix.rating = Rating[rating]
-        netflix.duration = row['duration']
-        netflix.listed_in = row['listed_in']
-        netflix.description = row['description']
-        netflix.date_added = row['date_added']
-        records.append(netflix)
+        records.append(Netflix(title=row['title'], type_of=Content_Type[type_of], director=str(row['director']), cast=str(row['cast']), country=str(row['country']), release_year=row['release_year'], rating=Rating[rating], duration=row['duration'], listed_in=row['listed_in'], description=row['description'], date_added=str(row['date_added'])))
 
     return records
 
 if __name__ == "__main__":
     db_id = "Netflix"
     url = "netflix.csv"
-    #client = WOQLClient("http://127.0.0.1:6363")
-    #client.connect()
 
-    team = "team"
-    client = WOQLClient("https://cloud.terminusdb.com/team/")
-
-    client.connect(team=team, use_token=True)
+    team = "mariog"
+    client = WOQLClient("https://cloud.terminusdb.com/mariog/")
     
     try:
-        client.create_database(db_id, team=team, label = "Netflix Graph", description = "Create a graph with Netflix data")
+        client.connect(team=team, use_token=True)
+        client.create_database(db_id, label = "Netflix Graph", description = "Create a graph with Netflix data")
     except Exception:
-        client.set_db(db_id)
-    client.insert_document(my_schema.to_dict(),
-                           graph_type="schema",
-                           commit_msg="I am checking in the schema")
+        client.connect(db=db_id, team=team, use_token=True)
+
+    schema.commit(client, commit_msg = "Adding Netflix Schema")
+    
     insert_data(client, url)
-    results = client.get_all_documents(graph_type="instance", count=10)
+    results = client.get_all_documents(count=10)
     print("\nRESULTS\n", list(results))
