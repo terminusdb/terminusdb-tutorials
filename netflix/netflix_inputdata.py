35082dba621332c8c48324360cf6f381e4e73ba1
Cheukting <cheukting.ho@gmail.com>
2022-05-24 21:42:09 +0100

    adding new stuff

--- netflix/netflix_inputdata.py
+++ netflix/netflix_inputdata.py
@@ -1,63 +1,11 @@
-from typing import  Set, Optional
 from terminusdb_client import WOQLClient
-from terminusdb_client.woqlschema.woql_schema import (
-    DocumentTemplate,
-    EnumTemplate,
-    WOQLSchema,
-    LexicalKey,
-)
 
 import pandas as pd
 from tqdm import tqdm
 import tempfile
 import random
 
-schema = WOQLSchema()
-
-class Content(DocumentTemplate):
-    _schema = schema
-    title: str
-    type_of: "Content_Type"
-    director: Optional[str]
-    cast: Optional[str]
-    country_of_origin: Optional[str]
-    release_year: int
-    rating: "Rating"
-    duration: str
-    listed_in: str
-    description: str
-    date_added: Optional[str]
-
-class User(DocumentTemplate):
-    _schema = schema
-    _key = LexicalKey(keys="id")
-    _base = "User"
-    id : str
-    watched_contents: Set["Content"]
-
-class Content_Type(EnumTemplate):
-    _schema = schema
-    TV_Show = "TV Show"
-    Movie = "Movie"
-
-class Rating(EnumTemplate):
-    _schema = schema
-    TV_MA = "TV-MA"
-    R = ()
-    PG_13 = "PG-13"
-    TV_14 = "TV-14"
-    TV_PG = "TV-PG"
-    NR = ()
-    TV_G = "TV-G"
-    TV_Y = "TV-Y"
-    TV_Y7 = "TV-Y7"
-    TY = ()
-    TY_7 = "TY-7"
-    PG = ()
-    G = ()
-    NC_17 = "NC-17"
-    TV_Y7_FV = "TV-Y7-FV"
-    UR = ()
+from schema import Content, User
 
 def insert_content_data(client, url):
     df = pd.read_csv(url, chunksize=1000)
@@ -153,18 +101,6 @@ def time_travel(client):
 if __name__ == "__main__":
     db_id = "Netflix"
     url = "netflix.csv"
-
-    # TODO: change the team name
-    # team = "<TEAM_NAME>"
-    # client = WOQLClient("https://cloud.terminusdb.com/"+team)
-
-    # try:
-    #     client.connect(team=team, use_token=True)
-    #     client.create_database(db_id, label = "Netflix Graph", description = "Create a graph with Netflix data")
-    # except Exception:
-    #     client.connect(db=db_id, team=team, use_token=True)
-    #
-    # schema.commit(client, commit_msg = "Adding Netflix Schema")
 
     client = WOQLClient("http://127.0.0.1:6363/")
     client.connect(db=db_id)
---
+++ netflix/netflix_recommendation.py
@@ -0,0 +1,6 @@
+from terminusdb_client import WOQLClient
+
+import pandas as pd
+from tqdm import tqdm
+
+from schema import Relations
