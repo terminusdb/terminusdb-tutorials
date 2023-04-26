11fc40b7f265e49316008a214c57e16fe759331c
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-04-26 14:17:27 +0200

    Adding ingest object construction

--- supply-chain/ingest.py
+++ supply-chain/ingest.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 from terminusdb_client import Client
-# import re
-# import csv
-# import json
-# import math
-# import os
+import re
+import json
 import urllib.parse
 import openpyxl
 import itertools
+import infer_schema
+import datetime
 
 team = "admin" # os.environ['TERMINUSDB_TEAM']
 team_quoted = urllib.parse.quote(team)
@@ -20,46 +19,93 @@ client.connect(team=team)
 dbid = "supply_chain"
 label = "Supply Chain"
 description = "A knowledge graph of supply chain information."
-prefixes = {'@base' : 'http://lib.terminusdb.com/SupplyChain/',
-            '@schema' : 'http://lib.terminusdb.com/SupplyChain#'}
-
 
 # Give the location of the file
 path = "data/SupplyChainLogisticsProblem.xlsx"
 
-# To open the workbook
-# workbook object is created
-wb_obj = openpyxl.load_workbook(path)
+def base_type(ty):
+    return ty[0:3] == 'xsd'
 
-type_map = {
-    's' : 'xsd:string',
-    'd' : 'xsd:dateTime',
-    'n' : 'xsd:decimal'
-}
+def import_schema(client):
+    with open('supply_chain.json', 'r') as f:
+        schema = json.load(f)
+    client.insert_document(schema, graph_type='schema', compress='never', full_replace=True)
+    return schema
 
-object_types = ['']
-objects = []
-for sheet_name in wb_obj.sheetnames:
-    object_type = sheet_name
-    property_types = []
-    # Collect header
-    for i in itertools.count(start=1):
-        property_type = sheet_obj.cell(row = 1, column = i).value
-        if property_type == None:
-            break
+def generate_schema_dict(schema):
+    schema_dict = {}
+    for sobj in schema:
+        if sobj['@type']== '@context':
+            key = '@context'
         else:
-            property_types.append(property_type)
-    print(property_types)
-    for i, property_type in enumerate(property_types, 1):
-        obj = { '@type' : property_type }
-        for j in itertools.count(start=2):
-            print(f"{i} {j}")
-            value_cell = sheet_obj.cell(row = j, column = i)
-            dt = value_cell.data_type
-            value = value_cell.value
-            print(f"{object_type}.{property_type}: {value}:{dt}")
-            if value == None:
+            key = sobj['@id']
+        schema_dict[key] = sobj
+
+    return schema_dict
+
+def import_data(client,schema):
+    objects = []
+    stubs = {}
+    wb_obj = openpyxl.load_workbook(path)
+    for sheet_name in wb_obj.sheetnames:
+        print(sheet_name)
+        object_type = sheet_name
+        sheet_obj = wb_obj[sheet_name]
+        cls = schema[object_type]
+        property_types = []
+        # Collect header
+        for i in itertools.count(start=1):
+            property_type = sheet_obj.cell(row = 1, column = i).value
+            if property_type == None:
                 break
             else:
-                pass
-        objects.append(obj)
+                property_types.append(property_type)
+        for j in itertools.count(start=2):
+            obj = { '@type' : object_type }
+            value = None
+            for i, property_type in enumerate(property_types, 1):
+                #print(f"{i} {j}")
+                value_cell = sheet_obj.cell(row = j, column = i)
+                value = value_cell.value
+                if value == None:
+                    break
+                else:
+                    rng = cls[property_type]
+                    #print(rng)
+                    #print(f"property_type: {property_type} range: {rng} value: {value}")
+                    if base_type(rng):
+                        if 'xsd:dateTime' == rng:
+                            obj[property_type] = value.isoformat()
+                        else:
+                            obj[property_type] = value
+                    else:
+                        matches = re.match(infer_schema.OID_REGEXP, value)
+                        if matches:
+                            matchdict = matches.groupdict()
+                            for key in matchdict:
+                                if matchdict[key]:
+                                    stubs[value] = key
+                            obj[property_type] = { '@ref' : value }
+            if value == None:
+                break
+            objects.append(obj)
+    for oid in stubs:
+        object_type = stubs[oid]
+        objects.append({'@type' : object_type, '@capture' : oid })
+    with open('objects.json', 'w') as f:
+        f.write(json.dumps(objects))
+    client.insert_document(objects, compress='never')
+
+if __name__ == "__main__":
+    exists = client.has_database(dbid)
+
+    if exists:
+        client.delete_database(dbid, team=team, force=True)
+
+    client.create_database(dbid,
+                           team,
+                           label=label,
+                           description=description)
+    schema = import_schema(client)
+    import_data(client, generate_schema_dict(schema))
+
